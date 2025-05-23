812e9f37dc0837afd5fb8f06baa0241dd366b83d
Carlos Santos <casantosmu@gmail.com>
2025-05-23 22:56:03 +0200

    Add `FloatingButton`

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,33 +1,10 @@
-import { useTranslation } from "react-i18next";
+import { Plus } from "lucide-react";
+import FloatingButton from "./components/FloatingButton";
 
 export default function App() {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng).catch(console.error);
-  };
-
   return (
-    <>
-      <h1>{t("welcomeMessage")}</h1>
-      <div>
-        <button
-          type="button"
-          onClick={() => {
-            changeLanguage("en");
-          }}
-        >
-          English
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            changeLanguage("es");
-          }}
-        >
-          Español
-        </button>
-      </div>
-    </>
+    <FloatingButton>
+      <Plus className="h-6 w-6" />
+    </FloatingButton>
   );
 }
---
+++ apps/frontend/src/components/FloatingButton.tsx
@@ -0,0 +1,12 @@
+import type { PropsWithChildren } from "react";
+
+export default function FloatingButton({ children }: PropsWithChildren) {
+  return (
+    <button
+      type="button"
+      className="fixed bottom-6 right-6 p-4 rounded-full bg-gray-800 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors"
+    >
+      {children}
+    </button>
+  );
+}
