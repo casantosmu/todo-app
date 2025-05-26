6d3343cc9d0509c1af686ba8c8235c96b36a29d4
Carlos Santos <casantosmu@gmail.com>
2025-05-26 19:27:47 +0200

    Add `Modal`

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import { Plus } from "lucide-react";
+import { useState } from "react";
 import Container from "./components/Container";
 import FloatingButton from "./components/FloatingButton";
+import Modal from "./components/Modal";
 
 export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Container>
-      <FloatingButton>
+      <FloatingButton onClick={handleOpenModal}>
         <Plus className="h-6 w-6" />
       </FloatingButton>
+
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        ¡Hola mundo!
+      </Modal>
     </Container>
   );
 }
--- apps/frontend/src/components/FloatingButton.tsx
+++ apps/frontend/src/components/FloatingButton.tsx
@@ -1,9 +1,18 @@
-import type { PropsWithChildren } from "react";
+import type { MouseEventHandler, ReactNode } from "react";
 
-export default function FloatingButton({ children }: PropsWithChildren) {
+interface FloatingButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  children?: ReactNode | undefined;
+}
+
+export default function FloatingButton({
+  onClick,
+  children,
+}: FloatingButtonProps) {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="fixed bottom-6 right-6 p-4 rounded-full bg-gray-800 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-colors"
     >
       {children}
