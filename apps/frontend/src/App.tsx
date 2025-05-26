8a21d03edbc9dfcf555d33d8ebdd81252b0ca546
Carlos Santos <casantosmu@gmail.com>
2025-05-26 17:54:17 +0200

    Add `Container`

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Plus } from "lucide-react";
+import Container from "./components/Container";
 import FloatingButton from "./components/FloatingButton";
 
 export default function App() {
   return (
-    <FloatingButton>
-      <Plus className="h-6 w-6" />
-    </FloatingButton>
+    <Container>
+      <FloatingButton>
+        <Plus className="h-6 w-6" />
+      </FloatingButton>
+    </Container>
   );
 }
