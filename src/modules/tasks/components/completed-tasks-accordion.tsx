0a48d71ce79523f1c46f57609000bb6e07516186
Carlos Santos <casantosmu@gmail.com>
2025-07-23 21:32:30 +0200

    Remove framer-motion

--- src/modules/tasks/components/completed-tasks-accordion.tsx
+++ src/modules/tasks/components/completed-tasks-accordion.tsx
@@ -4,37 +4,8 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { AnimatePresence, motion, type Variants } from "framer-motion";
 import { useCompletedTasks } from "../hooks/use-tasks";
 import { TaskItem } from "./task-item";
-
-const taskItemFramerVariants: Variants = {
-  initial: {
-    opacity: 0,
-    y: -20,
-    height: 0,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
-    height: "auto",
-    transition: {
-      type: "tween",
-      ease: "easeOut",
-      duration: 0.3,
-    },
-  },
-  exit: {
-    opacity: 0,
-    height: 0,
-    y: -10,
-    transition: {
-      type: "tween",
-      ease: "easeIn",
-      duration: 0.2,
-    },
-  },
-};
 
 export const CompletedTasksAccordion = () => {
   const { data: tasks = [] } = useCompletedTasks();
@@ -51,20 +22,11 @@ export const CompletedTasksAccordion = () => {
         </AccordionTrigger>
         <AccordionContent>
           <ul className="space-y-2">
-            <AnimatePresence initial={false}>
-              {tasks.map((todo) => (
-                <motion.li
-                  key={todo.id}
-                  variants={taskItemFramerVariants}
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  layout
-                >
-                  <TaskItem task={todo} />
-                </motion.li>
-              ))}
-            </AnimatePresence>
+            {tasks.map((todo) => (
+              <li key={todo.id}>
+                <TaskItem task={todo} />
+              </li>
+            ))}
           </ul>
         </AccordionContent>
       </AccordionItem>
--- src/modules/tasks/components/task-list.tsx
+++ src/modules/tasks/components/task-list.tsx
@@ -1,34 +1,5 @@
-import { AnimatePresence, motion, type Variants } from "framer-motion";
 import { usePendingTasks } from "../hooks/use-tasks";
 import { TaskItem } from "./task-item";
-
-const taskItemFramerVariants: Variants = {
-  initial: {
-    opacity: 0,
-    y: -20,
-    height: 0,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
-    height: "auto",
-    transition: {
-      type: "tween",
-      ease: "easeOut",
-      duration: 0.3,
-    },
-  },
-  exit: {
-    opacity: 0,
-    height: 0,
-    y: -10,
-    transition: {
-      type: "tween",
-      ease: "easeIn",
-      duration: 0.2,
-    },
-  },
-};
 
 export const TaskList = () => {
   const { data: tasks = [] } = usePendingTasks();
@@ -39,20 +10,11 @@ export const TaskList = () => {
 
   return (
     <ul className="space-y-2">
-      <AnimatePresence initial={false}>
-        {tasks.map((todo) => (
-          <motion.li
-            key={todo.id}
-            variants={taskItemFramerVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            layout
-          >
-            <TaskItem task={todo} />
-          </motion.li>
-        ))}
-      </AnimatePresence>
+      {tasks.map((todo) => (
+        <li key={todo.id}>
+          <TaskItem task={todo} />
+        </li>
+      ))}
     </ul>
   );
 };
